4394bf2830eccb25d58019382f7b44fe599c1ef0
idvysotskiy <vysockij.s@i20.team>
2024-05-07 17:32:48 +0700

    add test_items.py from 3.6 lesson

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+class TestProductPage:
+    def test_add_to_basket_btn(self, browser):
+        link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        button = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form > button')
+        assert button is not None, 'Button not found'
+
